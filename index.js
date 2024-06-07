ee54c2b51d3c032c98ff04386d9e4f9b0d984e03
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-07 20:00:30 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const PORT = 3000
+const app = express()
+
+const chat = [
+    {human: "Hola, soy Simón"},
+    {system: "Hola Simón, ¿en qué puedo ayudarte"},
+    {human: "¿Cómo configuro un almacén?"},
+    {system: ""}
+]
+
+
+app.listen(PORT, ()=> console.log('Server express levantado'))
+
+app.get('/', (req, res)=> res.send("Hola mundo desde express"))
+
+
+app.get('/api/history', (req, res)=> res.json(chat))
+
+app.post('/api/chat', (req, res)=> {
+    const human_message = req.body.human_message
+    //const history = req.body.history
+
+    res.json(human_message)
+})
