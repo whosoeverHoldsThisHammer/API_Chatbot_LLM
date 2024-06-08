36ee02782ad256ef841a409a79a5b10af560ebfc
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-07 22:10:34 -0300

    Se agrega llamada al llm desde controller

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,11 +1,34 @@
+import { ChatOpenAI } from "@langchain/openai"
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+import { StringOutputParser } from "@langchain/core/output_parsers" 
 
 // GET
 const generateAnswer = async (req, res) => {
     try {
         // const data = req.body
+        // res.send({data})
         console.log("Estoy acá")
-        res.json("Hola")
-        //res.send({data})
+        //res.json("Hola")
+
+        const model = new ChatOpenAI({
+            modelName: "gpt-3.5-turbo",
+            temperature: 0
+        });
+        
+        const prompt = ChatPromptTemplate.fromMessages([
+            ["system", "Generá un chiste a partir de la palabra que te pase el usuario"],
+            ["human", "{input}"]
+        ])
+        
+        const parser = new StringOutputParser();
+        const chain = prompt.pipe(model).pipe(parser)
+        
+        const answer = await chain.invoke({
+            input: "perro"
+        })
+
+        res.json(answer)
+        
     } catch (error) {
         
     }
---
+++ openai.js
@@ -0,0 +1,4 @@
+import { ChatOpenAI } from "@langchain/openai"
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+import { StringOutputParser, CommaSeparatedListOutputParser } from "@langchain/core/output_parsers" 
+
